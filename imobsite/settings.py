c8f795c7d433ae92f63ad61035d31c45ec08c068
Guilherme Schilling <guinoeischilling@gmail.com>
2024-05-14 07:39:14 -0300

    env update

--- imobsite/settings.py
+++ imobsite/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 
 
@@ -10,7 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
